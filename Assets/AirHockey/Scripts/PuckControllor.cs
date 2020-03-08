d7ee2d6f0e1207163baf9e0aabdb5c2522a16682
SuzuSho <ex>
2020-03-09 03:03:16 +0900

    ignoreファイルの追加

---
+++ Assets/AirHockey/Scripts/PuckControllor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuckControllor : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 player_pos = transform.position;
+        player_pos.x = Mathf.Clamp(player_pos.x, -140, 140);
+        player_pos.z = Mathf.Clamp(player_pos.z, -330, 330);
+        transform.position = player_pos;
+    }
+}
---
+++ Assets/AirHockey/Scripts/StrikerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class StrikerController: MonoBehaviour {
+	
+	public float scroll_speed = 0.01f;
+	public float min_scroll = 0.01f;
+	public float max_scroll = 1.0f;
+	private Vector3 startPostion;
+	private Vector3 preMousePosition;
+	private Vector3 startMousePosition;
+	private Rigidbody _rb;
+	public float speed = 0.5f;
+
+	void Start () {
+		startPostion = transform.position;
+		startMousePosition = Input.mousePosition;
+		_rb = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown (1)) {
+			startMousePosition = Input.mousePosition;
+		}
+		float scroll = Input.GetAxis ("Mouse ScrollWheel");
+		ChangeSpeed (scroll);
+		var mousePosition = Input.mousePosition;
+		Move (mousePosition);
+		preMousePosition = mousePosition;
+	}
+
+	private void Move(Vector3 currentMousePosition) {
+        Vector3 position = (currentMousePosition - startMousePosition);
+        position.z = position.y - 250.0f / speed;
+		_rb.MovePosition(position * speed);
+		if(_rb.velocity.magnitude > 1) {
+            _rb.velocity = _rb.velocity.normalized;
+		}
+		Vector3 player_pos = transform.position;
+		player_pos.x = Mathf.Clamp(player_pos.x, -135, 135);
+		player_pos.z = Mathf.Clamp(player_pos.z, -300, 0);
+		transform.position = player_pos;
+	}
+		
+	private void ChangeSpeed(float scroll) {
+		if (scroll < 0) {
+			speed -= scroll_speed;
+		} else if (scroll > 0) {
+			speed += scroll_speed;
+		}
+		speed = Mathf.Clamp (speed, min_scroll, max_scroll);
+	}
+}
